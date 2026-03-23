76a93bcd9ef5b79c86db4195054f1b5cb916c66e
Conde <157609877+fvconde@users.noreply.github.com>
2026-03-23 08:08:09 -0300

    feat - add loading when onOptmize event is called

--- frontend/src/app/components/config-panel/config-panel.component.ts
+++ frontend/src/app/components/config-panel/config-panel.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { OptimizeRequest } from '../../models/optimize-request';
@@ -18,6 +18,7 @@ export class ConfigPanelComponent {
   max_capacity: number = 100;
   max_distance: number = 300;
 
+  @Input() isLoading: boolean = false;
   @Output() optimize = new EventEmitter<OptimizeRequest>();
 
   onSubmit() {
